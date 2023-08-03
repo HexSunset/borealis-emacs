9f2ee1f5c191675907bfccffade496ee08be5222
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-08-03 22:19:58 +0300

    Make sure theme installed, use-package macro sets it

--- init.el
+++ init.el
@@ -33,9 +33,6 @@
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
 
-;; Theme
-(load-theme 'gruber-darker t)
-
 (add-to-list 'default-frame-alist
 	     '(font . "Iosevka-15"))
 
@@ -50,6 +47,8 @@
 
 (use-package smex
   :bind ("M-x" . smex))
+(use-package gruber-darker-theme
+  :init (load-theme 'gruber-darker t))
 (use-package compile
   :bind ("C-x c" . compile))
 (use-package ido
@@ -142,7 +141,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '( use-package org-roam eglot company rust-mode smex magit which-key)))
+   '(gruber-darker-theme use-package org-roam eglot company rust-mode smex magit which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
