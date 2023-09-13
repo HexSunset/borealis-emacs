15cb42085dcf03c43fd7b31536e1bfde85504d64
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-09-13 12:37:58 +0300

    Line modification functions

--- borealis-fn.el
+++ borealis-fn.el
@@ -18,6 +18,33 @@
   (forward-line -1)
   (indent-for-tab-command))
 
+(defun clone-line-below ()
+  "Create a copy of the current line below"
+  (interactive)
+  (beginning-of-line)
+  (kill-line)
+  (yank)
+  (newline)
+  (yank))
+
+(defun move-line-up ()
+  "Swap the current line and the line above it"
+  (interactive)
+  (beginning-of-line)
+  (kill-line 1)
+  (previous-line)
+  (yank)
+  (backward-char))
+
+(defun move-line-down ()
+  "Swap the current line and the line above it"
+  (interactive)
+  (beginning-of-line)
+  (kill-line 1)
+  (next-line)
+  (yank)
+  (backward-char))
+
 (defun make-directory-if-not (DIR)
   "Create DIR if it doesn't exist and return it's name"
   (if (not (file-directory-p DIR)) (make-directory DIR))
