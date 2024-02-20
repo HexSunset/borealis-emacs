19c558f08a7833e824d0b6cc374ce2682074aff1
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-02-20 19:50:08 +0200

    Restart, declutter, zen, feng shui

--- init.el
+++ init.el
@@ -43,54 +43,15 @@
 ;; -- PACKAGES --
 ;; --------------
 
-(require 'use-package)
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(require 'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'execute-extended-command)
 
-(use-package smex
-  :bind (("M-x" . smex)
-	 ("M-X" . smex-major-mode-commands)))
-(use-package hydra)
-(use-package gruber-darker-theme
-  :init (load-theme 'gruber-darker t))
-(use-package compile
-  :init (setq compile-command nil)
-  :bind ("C-x c" . compile))
-;;(use-package fancy-compilation
-;;  :commands (fancy-compilation-mode))
-(with-eval-after-load 'compile
-  (fancy-compilation-mode))
-(use-package vterm)
-(use-package ido
-  :config
-  (setq ido-ignore-buffers '("\\` " "\\`*[:alnum:]*\\`*"))
-  (ido-everywhere))
-(use-package company
-  :config
-  (setq company-idle-delay nil)
-  (global-company-mode)
-  :bind ("C-<tab>" . company-complete))
-(use-package eglot)
-(use-package dired-x)
-(use-package magit)
-(use-package which-key
-  :config
-  (which-key-mode))
-(use-package calendar
-  :config
-  (setq calendar-week-start-day 1))
-(use-package org
-  :init
-  (setq org-directory (make-directory-if-not "~/org"))
-  :config
-  (setq org-agenda-files (directory-files org-directory))
-  (setq org-default-notes-file (concat org-directory "/notes.org"))
-  (setq org-agenda-start-on-weekday nil)
-  (setq org-agenda-skip-scheduled-if-done t)
-  (setq org-agenda-skip-deadline-if-done t))
-(use-package slime
-  :init (setq inferior-lisp-program "sbcl"))
-(use-package rust-mode)
+(require 'gruber-darker-theme)
+(load-theme 'gruber-darker t)
+
+(require 'ido)
+(ido-everywhere 1)
 
 
 ;; --------------
@@ -98,7 +59,6 @@
 ;; --------------
 ;; settings that don't belong with a specific package
 
-;; I'm tired of having to write out yes and no
 (setq use-short-answers t)
 
 (setq dired-listing-switches "-alh")
@@ -134,20 +94,11 @@
 (global-unset-key (kbd "C-M-n"))
 (global-set-key (kbd "C-M-n") 'move-text-down)
 
-
 (global-set-key (kbd "C-x C-r") 'sudo-find-file)
 
 (global-set-key (kbd "<C-return>") 'open-line-below)
 
 (global-set-key (kbd "<C-M-return>") 'open-line-above)
-
-(global-unset-key (kbd "C-x C-n")) ;set-goal-column
-(global-set-key (kbd "C-x C-n") 'revert-buffer-quick)
-
-;; (global-unset-key (kbd "C-h"))
-;; (global-set-key (kbd "C-h") 'backward-delete-char)
-;; (global-unset-key (kbd "M-h")) ;mark-paragraph
-;; (global-set-key (kbd "M-h") 'backward-kill-word)
 
 
 ;; ------------
@@ -160,7 +111,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(hydra vterm fancy-compilation smartparens rainbow-delimiters slime paredit markdown-preview-mode markdown-mode gruber-darker-theme use-package org-roam eglot company rust-mode smex magit which-key)))
+   '(markdown-mode gruber-darker-theme company rust-mode smex magit which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
