7a6e7bb235e33c40526210d6a26cde5004e154dd
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-03-15 15:25:35 +0200

    Use yasnippet

--- init.el
+++ init.el
@@ -98,6 +98,8 @@
 
 (use-package magit)
 
+(use-package yasnippet)
+
 (use-package company
   :config
   (setq company-idle-delay nil)
@@ -142,7 +144,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(use-package markdown-mode gruber-darker-theme company rust-mode smex magit which-key)))
+   '(yasnippet use-package markdown-mode gruber-darker-theme company rust-mode smex magit which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
