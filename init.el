42d764929dd91e55cf558c3f2e51366324be711a
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-03-21 09:48:01 +0200

    use eglot

--- init.el
+++ init.el
@@ -100,6 +100,8 @@
 
 (use-package yasnippet)
 
+(use-package eglot)
+
 (use-package company
   :config
   (setq company-idle-delay nil)
@@ -144,7 +146,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet use-package markdown-mode gruber-darker-theme company rust-mode smex magit which-key)))
+   '(eglot yasnippet use-package markdown-mode gruber-darker-theme company rust-mode smex magit which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
