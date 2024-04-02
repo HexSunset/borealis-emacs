74c3af0bf4a8ee7b80327d3c2e23e1c5ae560ef6
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-04-02 22:57:17 +0300

    Simpler font specification

--- init.el
+++ init.el
@@ -37,10 +37,11 @@
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
 
-(set-face-attribute 'fixed-pitch nil :family "Iosevka" :height 150)
-(set-face-attribute 'fixed-pitch-serif nil :family "Iosevka Slab" :height 150)
-(set-face-attribute 'variable-pitch nil :family "Iosevka Aile" :height 150)
-(set-face-attribute 'default nil :family "Iosevka" :height 150)
+;;(set-face-attribute 'fixed-pitch nil :family "Iosevka" :height 150)
+;;(set-face-attribute 'fixed-pitch-serif nil :family "Iosevka Slab" :height 150)
+;;(set-face-attribute 'variable-pitch nil :family "Iosevka Aile" :height 150)
+;;(set-face-attribute 'default nil :font "Iosevka" :height 150)
+(set-frame-font "Iosevka-15" nil t)
 
 
 ;; --------------
